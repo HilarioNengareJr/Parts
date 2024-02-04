7132a38ddca6625adf433da1ff7df787e9526c08
Hilario Nengare <hnengare@gmail.com>
2024-02-04 14:31:11 +0300

    exercise 1

--- src/Content.jsx
+++ src/Content.jsx
@@ -4,13 +4,13 @@ const Content = (props) => {
     return (
         <>
             <p>
-                {props.part1} {props.exercise1}
+                {props.part1} {props.exercises1}
             </p>
             <p>
-                {props.part2} {props.exercise2}
+                {props.part2} {props.exercises2}
             </p>
             <p>
-                {props.part3} {props.exercise3}
+                {props.part3} {props.exercises3}
             </p>
         </>
     );
--- src/Total.jsx
+++ src/Total.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Total = (props) => {
 
     return (
-        <> <p>Number of exercises {props.exercise + props.exercise + props.exercise}</p></>
+        <> <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p></>
     );
 }
 
