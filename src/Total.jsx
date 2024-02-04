7d2df95a5bd34a46543b07dbe0006d82d98fbe35
Hilario Nengare <hnengare@gmail.com>
2024-02-04 14:23:17 +0300

    React-redux course

---
+++ src/Total.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Total = (props) => {
+
+    return (
+        <> <p>Number of exercises {props.exercise + props.exercise + props.exercise}</p></>
+    );
+}
+
+export default Total;
