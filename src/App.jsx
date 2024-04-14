09c57141d4d6c84498e928f6a6698449ba900ddf
Hilario Nengare <hnengare@gmail.com>
2024-04-14 07:02:25 +0300

    Part 2: Forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,83 @@
-import React from 'react';
-import Total from './Total';
-import Header from './Header';
-import Content from './Content';
+import { useState } from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '555-7777' }
+  ])
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [searchInput, setSearchInput] = useState('');
 
+  const handleNewName = (event) => {
+    console.log(newName);
+    setNewName(event.target.value);
+  }
 
-  /*
-    <Header course={course} />
-    <Content ... />
-    <Total ... />
-  */
+  const handleNewNumber = (event) => {
+    console.log(newNumber);
+    setNewNumber(event.target.value);
+  }
+
+  const filter = (event) => {
+    console.log(searchInput);
+    setSearchInput(event.target.value);
+  }
+
+  const handleAddition = (event) => {
+    event.preventDefault();
+    const newPerson = { name: newName, number: newNumber };
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    else {
+      setPersons([...persons, newPerson]);
+      setNewName('');
+      setNewNumber('');
+    }
+  }
+
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <h2>Phonebook</h2>
+
+      <div>
+        filter shown with <input value={searchInput} onChange={filter} />
+      </div>
+
+      <h2>
+        Add a new
+      </h2>
+      <form onSubmit={handleAddition}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNewNumber} />
+        </div>
+        <div>
+          <button type="submit"> add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {
+          searchInput ? filteredPersons.map(person => (
+            <li key={person.name} style={{ display: 'flex-inline' }}>
+              {person.name} {person.number}
+            </li>
+          )) : persons.map(person => (
+            <li key={person.name} style={{ display: 'flex-inline' }}>
+              {person.name} {person.number}
+            </li>
+          ))
+        }
+      </ul>
+
     </div>
   )
 }
